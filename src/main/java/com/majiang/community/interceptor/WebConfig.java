0f0b44bbe21d3014de48c8ff5be49907dbbaec21
geek-chen <geekChen@aliyun.com>
2021-03-12 21:37:00 +0800

    修复bug  新增问题详情页  新增个人属性页

---
+++ src/main/java/com/majiang/community/interceptor/WebConfig.java
@@ -0,0 +1,27 @@
+package com.majiang.community.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private SessionInterceptor sessionInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(sessionInterceptor).addPathPatterns("/**").excludePathPatterns("/resources/**");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+}
